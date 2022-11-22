c60b4d84b2fad3171100ecb5f599baadccbdf5a4
Mark van Aalst <markvanaalst@gmail.com>
2022-11-22 13:22:53 +0100

    refactored rich text profile

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,23 +1,7 @@
-import markdownStyles from "./markdown-styles.module.css";
 import { JSONContent } from "@tiptap/core";
 import { generateHTML } from "@tiptap/html";
 import { useMemo } from "react";
-import { Blockquote } from "@tiptap/extension-blockquote";
-import { Bold } from "@tiptap/extension-bold";
-import { Text } from "@tiptap/extension-text";
-import { BulletList } from "@tiptap/extension-bullet-list";
-import { Code } from "@tiptap/extension-code";
-import { CodeBlock } from "@tiptap/extension-code-block";
-import { Document } from "@tiptap/extension-document";
-import { HardBreak } from "@tiptap/extension-hard-break";
-import { Heading } from "@tiptap/extension-heading";
-import { HorizontalRule } from "@tiptap/extension-horizontal-rule";
-import { Italic } from "@tiptap/extension-italic";
-import { ListItem } from "@tiptap/extension-list-item";
-import { OrderedList } from "@tiptap/extension-ordered-list";
-import { Paragraph } from "@tiptap/extension-paragraph";
-import { Strike } from "@tiptap/extension-strike";
-import { Link } from "@tiptap/extension-link";
+import { richTextProfile } from "../lib/richTextConfiguration";
 
 type Props = {
   content: JSONContent;
@@ -25,24 +9,7 @@ type Props = {
 
 const PostBody = ({ content }: Props) => {
   const output = useMemo(() => {
-    return generateHTML(content, [
-      Document,
-      Paragraph,
-      Text,
-      Bold,
-      Heading,
-      ListItem,
-      Blockquote,
-      BulletList,
-      Code,
-      CodeBlock,
-      HardBreak,
-      HorizontalRule,
-      Italic,
-      OrderedList,
-      Strike,
-      Link,
-    ]);
+    return generateHTML(content, [richTextProfile]);
   }, [content]);
 
   return (
